59aec9991a1d38d3421650e54d0f71b7b6842ae1
MarColnaghi <marc.colnaghi@gmail.com>
2023-03-02 16:07:29 +0000

    Smaller fixes.
    -Added UI candy
    -Added sendtoserver(justification and coordinates at every press release)

--- Game.gd
+++ Game.gd
@@ -6,22 +6,29 @@ var time
 var centre
 var timeformovement
 var timeforjustification
-var PlayerName = "test"
-var scene = preload("res://Scenes/Player.tscn")
+var vector_array
+var coordinates
+var justificationtext = ""
 
+var scene = preload("res://Scenes/Player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	print(PlayerName)
+	print(Global.PlayerName)
 	centre = Global.get_viewport_rect().size/2
+	read_json_file("res://trialstructure.json")
+	
 	align_stuff()
 	start_trial()
 
-#func make_player(name):
-#	var instance = scene.instance()
-#	add_child(instance)
-
+func read_json_file(file_path):
+	var file = File.new()
+	file.open(file_path, File.READ)
+	var content_as_text = file.get_as_text()
+	var content_as_dictionary = parse_json(content_as_text)
+	return content_as_dictionary
+	
 func align_stuff():
 	$LineH.global_position.y = centre.y
 	$LineV.global_position.x = centre.x
@@ -30,6 +37,7 @@ func start_trial():
 	timeformovement = 5
 	$Q1.set_bbcode("[center]Trump or Obama[/center]")
 	$Q2.set_bbcode("[center]Orange or Blue[/center]")
+
 	$Justification.visible = false
 	$Justification_text.visible = false
 	$Player.can_move = true
@@ -44,7 +52,6 @@ func start_timer(trial_duration):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-	#print($Icon.global_position)
 
 func _on_Timer_timeout():
 	print("times up")
@@ -52,6 +59,9 @@ func _on_Timer_timeout():
 	$Timer.stop()
 	if $Timer.id == "justification":
 		start_trial()
+		justificationtext = $Justification.text
+		Global.sendtoserver(justificationtext, Global.PlayerName)
+		
 	elif $Timer.id == "start_trial":
 		justification()
 
@@ -66,4 +76,4 @@ func justification():
 func reset_position():
 	$Player.global_position = centre
 
-	
+
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,21 +1,26 @@
 extends Node2D
-
+var coordinates = [];
 var can_move = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var centre = Global.get_viewport_rect().size/2
 	self.global_position = centre
-
 
 func _input(event):
 	if event is InputEventScreenDrag and can_move :
 		self.global_position = event.position
 		self.scale = Vector2(1.2, 1.2)
 		#print(self.global_position)
-	#elif event is InputEventScreenTouch.ispressed():
+		
+	elif event is InputEventScreenTouch and !event.is_pressed():
+		self.scale = Vector2(1, 1)
+		coordinates = event.position
+		Global.sendtoserver(coordinates, Global.PlayerName)
+		
+		#event.is
 	#	self.scale = Vector2(1, 1)	
 	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	pass
