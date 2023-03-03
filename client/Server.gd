9107ed74e8505059e7ed22b14187c17d0c9684a4
Hyungju Jeon <hyungju.jeon@gmail.com>
2023-03-03 15:50:32 +0000

    user specific bot justification

--- client/Server.gd
+++ client/Server.gd
@@ -7,7 +7,7 @@ var q1_bot = ""
 var q2_bot = ""
 remote var show_botjustification = false
 remote var querydict = []
-
+ 
 var client
 
 # Enable dev mode by launching the game with -dev flag or "DEV=true" env variable.
